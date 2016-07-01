8b24db627456e85439a901913dd7c3a97c6fc9c7
giantas <aswapaul@gmail.com>
2016-07-01 23:10:56 +0300

    Use map-lambda

--- Map/maps.py
+++ Map/maps.py
@@ -1,27 +1,14 @@
 # Write a program that maps a list of words 
 # into a list of integers representing the lengths of the correponding words.
 
-def map_list(get_list):
-    """Returns a list of lengths of elements in input list"""
-    
-    if type(get_list) != list or len(get_list) == 0 or get_list is None:
-        return False
-    
-    int_list = []
-    
-    for i in get_list:
-        if type(i) != str:
-            return False
-        else:
-            int_list.append(len(i))
-        
-    return int_list
-
 def main():
     
-    word_list = input("Enter a list of words: ")
+    word_list = input("Enter a list of strings: ")
     
-    print map_list(word_list)
+    if type(word_list) != list or len(word_list) == 0 or word_list is None:
+        print False
+    else:
+        print map(lambda x: len(x), word_list)
 
 if __name__ == "__main__":
     main()
